8f2f930000bfb326cee38b79e61198bc682fb1b3
Craig Magras <whiteymcq-tip@craigs-imac.mynetworksettings.com>
2023-08-27 23:39:43 -0500

    added ch.5 exercises

---
+++ 5-Loops_Iterators/countdown2.rb
@@ -0,0 +1,10 @@
+# countdown2.rb
+
+x = gets.chomp.to_i
+
+until x < 0
+  puts x
+  x -= 1
+end
+
+puts "Done!"
---
+++ 5-Loops_Iterators/useful_loop.rb
@@ -0,0 +1,8 @@
+# useful_loop.rb
+
+i = 0
+loop do
+  i = i + 1
+  puts i
+  break # This will cause execution to exit the loop
+end
